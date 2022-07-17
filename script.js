d900b29ae0d5af1c9019179d4f5861aa834b93fe
Lt-AldoRaine <harambefallon@gmail.com>
2022-07-17 03:35:02 -0500

    Create function to play a round and decide winner for that round

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-let playerSelection;
 
 function getComputerChoice() {
-    const array = Array('rock', 'paper', 'scissors')
-    // Randomly pick rock, paper, or scissors from the array.
-    let choice = array[Math.floor(Math.random() * array.length)]
-    console.log(choice);
+    let options = Array('rock', 'paper', 'scissors');
+    computerChoice = options[Math.floor(Math.random() * options.length)];
+    console.log(`Computer chose: ${computerChoice}`);
+    return computerChoice;
 }
 
-function playRound(playerSelection, getComputerChoice) {
-
+function playerSelection() {
+    let choice = prompt('Rock, paper, or scissors? ').toLowerCase();
+    console.log(`You chose: ${choice}`);
+    return choice;
 }
-getComputerChoice();
+
+function playRound(playerChoice) {
+    let computerPlay = getComputerChoice();
+    let result;
+
+    if (playerChoice === computerPlay) {
+        result = 'Tie';
+        return result;
+    } else if (playerChoice === 'rock') {
+        if (computerPlay === 'paper') {
+            result = 'Lose';
+            return result;
+        } else {
+            result = 'Win';
+            return result;
+        }
+    } else if (playerChoice === 'paper') {
+        if (computerPlay === 'rock') {
+            result = 'Win';
+            return result;
+        } else {
+            result = 'Lose';
+            return result;
+        }
+    } else if (playerChoice === 'scissors') {
+        if (computerPlay === 'paper') {
+            result = 'Win';
+            return result;
+        } else {
+            result = 'Lose';
+            return result;
+        }
+    }
+}
+
+
+const playerChoice = playerSelection();
+console.log(playRound(playerChoice));
